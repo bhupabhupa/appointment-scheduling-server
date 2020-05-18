fee23e8b82e3890419e679551b29ae1c6455bba9
Bhupendra Tandel <bhupabhupa@gmail.com>
2020-05-19 02:47:31 +0530

    temp keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
-if(process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod');
-} else {
-    module.exports = require('./dev');
-}
+// if(process.env.NODE_ENV === 'production') {
+//     module.exports = require('./prod');
+// } else {
+//     module.exports = require('./dev');
+// }
 
 
-//module.exports = require('./prod');
+module.exports = require('./prod');
