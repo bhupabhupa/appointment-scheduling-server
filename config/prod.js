66f0b4e3a58f90f94d42ff99e4417f36444a6b6d
Bhupendra Tandel <bhupabhupa@gmail.com>
2020-05-19 00:26:58 +0530

    production change

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    JWT_KEY: process.env.JWT_KEY,
-    SendGridKey: process.env.SEND_GRID_KEY,
-    ClientURI: process.env.CLIENT_URI
+    mongoURI: 'mongodb+srv://root:root@generaldb-mwyww.mongodb.net/general-dev?retryWrites=true&w=majority',
+    JWT_KEY: 'IWANTTOCRACKITALL',
+    SendGridKey: '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',
+    ClientURI: 'https://morning-depths-21129.herokuapp.com/'
 };
